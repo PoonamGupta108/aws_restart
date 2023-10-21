5f4af634b9e2d7935a2b2cdcbbdfa99cd6c14fd0
PoonamGupta108 <84040850+PoonamGupta108@users.noreply.github.com>
2023-10-21 13:20:31 +0530

    Add files via upload
    commited

---
+++ categorize-values.py
@@ -0,0 +1,3 @@
+names = ['David', 'Peter', 'Michael', 'John', 'Bob']
+for i in range (len (names)):
+    print("{}.{}".format(i + 1, names[i]))
---
+++ string-data-type.py
@@ -0,0 +1,14 @@
+myString= "This is a string."
+print(myString)
+
+print(type(myString))
+print(myString + " is of the type " + str(type(myString)))
+firstString= "Rain"
+secondString= "Fall"
+thirdString= firstString + secondString
+print(thirdString)
+name = input("Your name?")
+print(name)
+color=("favorite color? ")
+animal=("favorite animal? ")
+print("{}, you like a {} {}!".format(name,color,animal))
